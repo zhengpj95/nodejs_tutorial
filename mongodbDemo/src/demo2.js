29c0f49b03fb160466b68a8b30b5efb582ee6fa4
ZhengAu <598359997@qq.com>
2020-04-25 11:07:16 +0800

    update

--- mongodbDemo/src/demo2.js
+++ mongodbDemo/src/demo2.js
@@ -3,4 +3,11 @@ const Student = require('./models/student');
 const globalFunc = require('./tools/globalFunc');
 
 Student.find({name: /11$/}, 'name age -_id', globalFunc.callback);
+
+/**
+ * 统计集合中文档个数
+ * countDocuments(condition, callback)  可以传条件
+ * estimatedDocumentCount(callback)     不需要条件的时候，这个效率更好
+ */
 Student.countDocuments({}, globalFunc.callback);
+Student.estimatedDocumentCount(globalFunc.callback);
