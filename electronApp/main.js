cd2c3d9131fa098340ccb47d4682d3dc9020dc34
ZhengAu <598359997@qq.com>
2020-04-27 18:48:27 +0800

    add menu

--- electronApp/main.js
+++ electronApp/main.js
@@ -12,9 +12,11 @@ function createWindow() {
 		backgroundColor: '#d5d3d3',
 		center: true
 	});
-
+	// load menu
+	require('./src/menu');
 	// and load the index.html of the app.
-	win.loadFile('index.html').catch(error => console.log(`Error: `, error));
+	win.loadFile('index.html')
+		.catch(error => console.log(`Error: `, error));
 	// win.loadURL('https://gitee.com/zhengpj').then(error => console.log('Error: ', error))
 
 	// Open the DevTools.
@@ -35,7 +37,7 @@ app.on('window-all-closed', () => {
 	// On macOS it is common for applications and their menu bar
 	// to stay active until the user quits explicitly with Cmd + Q
 	if (process.platform !== 'darwin') {
-		app.quit()
+		app.quit();
 	}
 });
 
@@ -43,7 +45,7 @@ app.on('activate', () => {
 	// On macOS it's common to re-create a window in the app when the
 	// dock icon is clicked and there are no other windows open.
 	if (BrowserWindow.getAllWindows().length === 0) {
-		createWindow()
+		createWindow();
 	}
 });
 
