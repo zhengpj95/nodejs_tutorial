97a385e27120de9a69d55e7ea80909eb4c96085b
ZhengAu <598359997@qq.com>
2020-05-08 22:52:05 +0800

    主界面添加跳转链接

---
+++ myElectronApp/src/gotoLink.js
@@ -0,0 +1,29 @@
+// if ($) {
+// 	console.log(`have loaded jquery`)
+// 	const $ = require('jquery');
+// }
+
+const {shell} = require('electron');
+
+let linkRedis = $('.linkRedis');
+let linkMongodb = $('.linkMongodb');
+let linkMongoose = $('.linkMongoose');
+let linkNodejsRedis = $('.linkNodejsRedis');
+let linkNodejsMongodb = $('.linkNodejsMongodb');
+let linkNodejsMongoose = $('.linkNodejsMongoose');
+
+let linkList = [linkRedis, linkMongodb, linkMongoose, linkNodejsRedis,
+	linkNodejsMongodb, linkNodejsMongoose];
+let urlList = ['https://redis.io/', 'https://docs.mongodb.com/manual/', 'https://mongoosejs.com/',
+	'https://www.npmjs.com/package/redis', 'https://www.npmjs.com/package/mongodb', 'https://www.npmjs.com/package/mongoose'];
+
+linkList.forEach((item, index) => {
+	if (item) {
+		item.click(function (e) {
+			e.preventDefault();
+			shell.openExternal(urlList[index])
+				.catch(err => console.log(`Error: ${err}`));
+		});
+	}
+})
+
