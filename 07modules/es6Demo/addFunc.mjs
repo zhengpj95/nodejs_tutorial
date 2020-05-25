9473e76a41ea0eeb8859e23802d02cd313d9346c
ZhengAu <598359997@qq.com>
2020-05-25 23:41:25 +0800

    nodejs es6modules

---
+++ 07modules/es6Demo/addFunc.mjs
@@ -0,0 +1,11 @@
+export function add(a, b) {
+	return a + b;
+}
+
+export function apply() {
+	console.log(`addFunc.mjs apply...`);
+}
+// export {
+// 	add,
+// 	apply
+// }
---
+++ 07modules/es6Demo/test.mjs
@@ -0,0 +1,6 @@
+import { add, apply } from './addFunc.mjs';//postfix name cannot ignore
+console.log(add(1, 11));
+apply();
+
+// how to run  
+// node --experimental-modules test.mjs
